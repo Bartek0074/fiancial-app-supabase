f33387f272c67677d62dbd226047d5a52c22d9e3
Bartek0074 <bartekdo123@gmail.com>
2026-09-06 13:21:16 +0200

    feat: added transaction list fallback

---
+++ app/dashboard/components/transaction-list-fallback.tsx
@@ -0,0 +1,64 @@
+import TransactionItem from '@/components/transaction-item';
+import TransactionSummaryItem from '@/components/transaction-summary-item';
+import Separator from '@/components/separator';
+import Skeleton from '@/components/skeleton';
+
+
+type TransactionListFallProps = {
+	className?: string;
+};
+
+export default function TransactionListFallback({
+	className,
+}: TransactionListFallProps) {
+	return (
+		<div className={`space-y-8 ${className}`}>
+			<div className={"space-y-4"}>
+				<TransactionSummaryItemSkeleton />
+				<TransactionItemSkeleton />
+				<TransactionItemSkeleton />
+				<TransactionItemSkeleton />
+				<TransactionItemSkeleton />
+			</div>
+			<div className={"space-y-4"}>
+				<TransactionSummaryItemSkeleton />
+				<TransactionItemSkeleton />
+				<TransactionItemSkeleton />
+				<TransactionItemSkeleton />
+				<TransactionItemSkeleton />
+			</div>
+		</div>
+	);
+}
+
+
+function TransactionItemSkeleton() {
+	return (
+		<div className='w-full flex items-center space-x-4'>
+			<div className='flex items-center grow'>
+				<Skeleton />
+			</div>
+			<div className='min-w-37.5 items-center hidden md:flex'>
+				<Skeleton />
+			</div>
+
+			<div className='min-w-17.5 text-right'><Skeleton /></div>
+
+			<div className='min-w-12.5 flex justify-end'><Skeleton /></div>
+		</div>
+	);
+}
+
+function TransactionSummaryItemSkeleton() {
+	return (
+		<div className='flex space-x-4'>
+			<div className='grow'>
+				<Skeleton />
+			</div>
+			<div className='min-w-17.5'>
+				<Skeleton />
+			</div>
+			<div className='min-w-12.5' />
+		</div>
+	);
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,14 @@
+import { Suspense } from 'react';
+
 import TransactionList from './components/transaction-list';
+import TransactionListFallback from './components/transaction-list-fallback';
 
 export default function Page() {
 	return (
 		<div>
-			<TransactionList />
+			<Suspense fallback={<TransactionListFallback />}>
+				<TransactionList />
+			</Suspense>
 		</div>
 	);
 }
